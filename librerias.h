95179c5f5ff1135a8688f88bc6d0e60b7e070d3a
andrew2077a <andres.garcia34@udea.edu.co>
2026-03-18 00:26:28 -0500

    problema_2

---
+++ librerias.h
@@ -0,0 +1,5 @@
+#ifndef LIBRERIAS_H
+#define LIBRERIAS_H
+
+
+#endif // LIBRERIAS_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+
+void problem_2(){
+    char arr[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char arre[200];
+    srand(time(0));
+    int i,x,con=0,e;
+    char *p=arr;
+    for (i=0;i<200;i++){
+        x = rand()%26;
+        arre[i]=arr[x];
+        cout<<arre[i];
+    }
+    cout<<endl;
+    for (i=0;i<26;i++){
+        for (e=0;e<200;e++){
+
+            if (*p==arre[e]){
+                con++;
+            }
+        }
+        cout<<*p<<":"<<con<<endl;
+        p++;
+        con=0;
+    }
+
+
+}
+int main(){
+    int x,i=0;
+    while (i<3){
+        cout<<"Ingrese un problema"<<endl;
+        cin>>x;
+        switch(x){
+        case 2:
+            problem_2();
+            break;
+        case 4:
+            break;
+        default:
+            cout<<"Ingreso un numero incorrecto"<<endl;
+        }
+        i+=1;
+    }
 }
